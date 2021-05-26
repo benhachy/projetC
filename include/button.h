8b88b79219b3bdaf9001c643a1c73468e2365f31
Amine El Bouzid <amine.el-bouzid@grenoble-inp.org>
2021-05-26 17:27:07 +0200

    Comments

--- include/button.h
+++ include/button.h
@@ -11,12 +11,35 @@
 #include "ei_types.h"
 #include "hw_interface.h"
 
+/**
+* @brief    REtourne une liste de points pour définir un arc 
+* @param    rayon : rayon de l'arc 
+*           start_angle, end_angle : angles de début et de fin de l'arc 
+*           center : centre de l'arc
+*
+*/
 ei_linked_point_t* arc(ei_point_t center, int rayon, float start_angle, float end_angle);
 
+/**
+* @brief    Retourne une liste de points pour définir un cercle
+* @param    rayon : rayon du cercle
+*           start_angle, end_angle : angles de début et de fin du cercle 
+*           center : centre du cercle
+*
+*/
 ei_linked_point_t* circle(ei_point_t center, int rayon, float start_angle, float end_angle);
 
+/**
+* @brief    Retourne une liste de points pour définir un cadre arrondi
+* @param    rayon : rayon des arrondis
+*           rect : rectangle du cadre
+*
+*/
 ei_linked_point_t* rounded_frame(ei_rect_t rect, int r, int parameter);
 
+/**
+* @brief   Dessine un button
+*/
 void  draw_button(  ei_surface_t surface,
                     ei_surface_t offscreen,
                     ei_rect_t* clipper,
