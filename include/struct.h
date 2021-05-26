2f3f88c46c2145dff67b42945bd1425f85f3f33f
Amine El Bouzid <amine.el-bouzid@grenoble-inp.org>
2021-05-26 17:35:23 +0200

    Comments

--- include/struct.h
+++ include/struct.h
@@ -11,6 +11,10 @@
 #include "ei_types.h"
 #include "hw_interface.h"
 
+/** 
+* @brief Définition d'une structure de segments
+*/
+
 typedef struct Segment Segment;
 struct Segment
 {
@@ -18,7 +22,9 @@ struct Segment
     ei_point_t end;
 };
 
-
+/** 
+* @brief Définition d'une structure de liste chainée de segments
+*/
 typedef struct Cell Cell;
 struct Cell
 {
@@ -26,11 +32,19 @@ struct Cell
     Cell *next;
 };
 
+/** 
+* @brief Définition d'une structure de pile
+*/
+
 typedef struct Stack Stack;
 struct Stack
 {
     Cell *first;
 };
+
+/** 
+* @brief Définition des types de la structure TC
+*/
 typedef struct tc_carac tc_carac;
 struct tc_carac {
     int y_max;
@@ -46,6 +60,9 @@ struct tc_cell
     struct tc_cell *next;
 };
 
+/** 
+* @brief Définition des types de la structure TCA
+*/
 typedef struct tca_cell tca_cell;
 struct tca_cell
 {
@@ -55,12 +72,22 @@ struct tca_cell
     struct tca_cell* next;
 };
 
+/** 
+* @brief  Ajoute un segment dans une pile de segments
+*/
 void add_stack(Stack *stack, Segment *segment);
 
+/** 
+* @brief  Retourne le segment enlevé de la pile
+*/ 
 Segment* pop(Stack *stack);
 
 void print_s_Array(Segment array[], int size);
 
+/** 
+* @brief  Retourne la structure Tc nécessaire au dessin du polygone 
+          défini par l'ensemble de points
+*/ 
 struct tc_cell* get_tc(ei_linked_point_t*	first_point);
 
 void* tca_append(struct tca_cell** tca, int y_max, float x_min, float m);
